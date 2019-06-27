70ff921288b7940f9b669c793c729fe88cbb2a4f
Sergey Filkin <fs@alternativa-pro.ru>
2019-06-27 20:44:48 +0300

    Telegram ressender

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ app.get("/test", (req, res) => {
   res.status(200).send({ result: "GET: /test" });
 });
 
-// test route
 app.get("/sendtext", (req, res) => {
   log.info("Send text: ");
   log.info(req.query);
@@ -58,6 +57,57 @@ app.get("/sendtext", (req, res) => {
 
   bot
     .sendMessage(data.chatId, data.text)
+    .then(data => {
+      // console.log(data);
+      return res.status(200).send({ result: "ok" });
+    })
+    .catch(err => {
+      log.error(err);
+      return res.status(400).send({ result: "Error send message", error: err });
+    });
+});
+
+app.get("/call", (req, res) => {
+  log.info("Skipped call: ");
+  log.info(req.query);
+
+  const dataSchema = joi.object().keys({
+    token: joi.string().required(),
+    bot: joi.string().required(),
+    chatId: joi.number().required(),
+    phone: joi.string().required(),
+    time: joi.string().required(),
+    duration: joi.string().required()
+  });
+
+  const data = req.query;
+  const validateResult = joi.validate(data, dataSchema);
+  if (validateResult.error) {
+    log.error(validateResult.error);
+    return res.status(400).send({ result: "validate error" });
+  }
+
+  if (data.token != config.token) {
+    return res.status(400).send({ result: "Incorrect sec.token!" });
+  }
+
+  const bot = new telegram(data.bot, {
+    polling: false,
+    request: {
+      agentClass: Agent,
+      agentOptions: {
+        socksHost: config.tgproxy_host,
+        socksPort: parseInt(config.tgproxy_port),
+        // If authorization is needed:
+        socksUsername: config.tgproxy_user,
+        socksPassword: config.tgproxy_password
+      }
+    }
+  });
+
+  const text = `Проп.звонок: ${data.phone} (${data.time} / ${data.duration}с.)`;
+  bot
+    .sendMessage(data.chatId, text)
     .then(data => {
       // console.log(data);
       return res.status(200).send({ result: "ok" });
